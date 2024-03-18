ef5cc7204b7b0c87a15e17ecd6115f4e7b968c83
Prabhu Kasinathan <pkasinathan@netflix.com>
2024-03-17 21:03:28 -0700

    hey-prathosh release v0.0.1

---
+++ app.py
@@ -0,0 +1,18 @@
+# import streamlit
+import streamlit as st
+
+# Title
+st.title('Hi, I am Prathosh!')
+
+# Header
+st.header('Welcome to my Page!')
+
+# About Me section
+st.markdown('### About Me:')
+st.markdown('Welcome to my website! I am Prathosh, a passionate 5th grader with a deep love for Godzilla. Soccer is my favorite sport, and I have a quirky habit of constantly changing wallpapers, although I am not quite sure why! Feel free to explore and discover more about me and my interests here.')
+
+st.markdown('---')
+
+# My Interests section
+st.markdown('### My Interests:')
+st.markdown("Coming soon...")
