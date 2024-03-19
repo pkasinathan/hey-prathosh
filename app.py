6bda639bac9a4a68ef5a0a861887a828f0cb44c7
Prabhu Kasinathan <pkasinathan@netflix.com>
2024-03-18 20:46:04 -0700

    hey-prathosh release v0.0.1

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import streamlit as st
 
 # Title
 st.title('Hi, I am Prathosh!')
+st.image('https://media3.giphy.com/media/MPxg9U887PS0B8XT4J/200w.gif?cid=6c09b952fk1klredu0gun45htyicmpkul8oxiax7dnjr2j2h&ep=v1_gifs_search&rid=200w.gif&ct=g', width=300)
 
 # Header
 st.header('Welcome to my Page!')
@@ -28,3 +29,8 @@ st.markdown('---')
 # My Projects section
 st.markdown('### My Hobbies:')
 st.markdown("Coming soon...")
+
+
+
+# To push to GitHub, run the following commands in the terminal:
+# bash git_push. https://prathosh.streamlit.app/
