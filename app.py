419ee398f6ca768a7f1a491f076b0c91b92f61a8
Prabhu Kasinathan <pkasinathan@netflix.com>
2024-03-18 21:16:29 -0700

    hey-prathosh release v0.0.1

--- app.py
+++ app.py
@@ -8,29 +8,40 @@ st.image('https://media3.giphy.com/media/MPxg9U887PS0B8XT4J/200w.gif?cid=6c09b95
 # Header
 st.header('Welcome to my Page!')
 
+
 # About Me section
 st.markdown('### About Me:')
 st.markdown('Welcome to my website! I am Prathosh, a passionate 5th grader with a deep love for Godzilla. Soccer is my favorite sport, and I have a quirky habit of constantly changing wallpapers, although I am not quite sure why! Feel free to explore and discover more about me and my interests here.')
-
 st.markdown('---')
+
 
 # My Interests section
 st.markdown('### My Interests:')
-st.markdown("I am working at this moment now...")
-
+st.markdown("As previously mentioned, I have a fondness for Godzilla. Drawing is one of my passions, and I have access to plenty of drawing materials because my mom is an artist. Additionally, I'm quite enthusiastic about playing soccer. It's a game I love dearly, and I make it a point to play it every day at school. Furthermore, I enjoy playing video games, particularly on my PlayStation5. Games like Lego Undercover are among my favorites. I also cherish the time spent playing with my friends. I have a sizable group of friends, and playing with them brings me immense joy. Moreover, I relish playing with my parents and my sister. I have a special affection for German Shepherds and puppies, although this doesn't mean I enjoy Paw Patrol; in fact, I strongly dislike it. I've been yearning to have a German Shepherd puppy named Muto for quite some time.")
+st.image('https://i.pinimg.com/originals/2c/48/8f/2c488ff14973e572d4e20ced00220aca.gif', width=300)
 st.markdown('---')
+
+# My Hobbies section
+st.markdown('### My Hobbies:')
+st.markdown("""I have a strong passion for writing fantasy books, drawing inspiration from movies featuring nearly immortal monsters and beasts.
+            Additionally, I'm a devoted fan of the Harry Potter series, finding great enjoyment in both reading and writing stories inspired by it.
+            While playing video games is enjoyable for me, I wouldn't necessarily consider it a hobby due to time constraints.
+            I find myself incredibly busy, primarily because of the significant amount of homework I have. 
+            Nevertheless, I prefer this busy schedule over attending Challenger, a rigorous school.""") 
+st.image('https://i.pinimg.com/originals/1a/af/16/1aaf162ed448874e6fe411356d029675.gif', width=300)
+st.markdown('---')
+
+
 
 # My Projects section
 st.markdown('### My Projects:')
 st.markdown("Coming soon...")
-
 st.markdown('---')
 
-# My Projects section
-st.markdown('### My Hobbies:')
-st.markdown("Coming soon...")
 
 
+# To test locally,
+# bash run_app.
 
 # To push to GitHub, run the following commands in the terminal:
 # bash git_push. https://prathosh.streamlit.app/
